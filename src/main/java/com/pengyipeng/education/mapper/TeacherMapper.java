53fb21a6a8d9ab3c3cb12d1d40d991023678b1b0
name <email@baidu.com>
2020-01-07 17:16:28 +0800

    胡婷婷上传项目文件

---
+++ src/main/java/com/pengyipeng/education/mapper/TeacherMapper.java
@@ -0,0 +1,62 @@
+package com.pengyipeng.education.mapper;
+
+import com.pengyipeng.education.model.vo.TeacherVo;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author 胡婷婷
+ * @version V1.0
+ * @Project: education
+ * @Package com.pengyipeng.education.mapper
+ * @date 2020/1/6 15:50 星期一
+ */
+@Mapper
+public interface TeacherMapper {
+
+    /**
+     * 根据条件返回教师信息
+     * @param map 查询条件
+     * @return 教师信息
+     */
+    List<TeacherVo> getTeacherMessage(Map<String, Object> map);
+
+    /**
+     * 修改教师的状态
+     * @param status 修改后状态
+     * @return 修改是否成功
+     */
+    int updateTeacherStatus(@Param("status") Integer status, @Param("tid") String tid);
+
+    /**
+     * 确认登录账号是否存在
+     * @param email 登录账号
+     * @param phone 手机号
+     * @return 用户ID
+     */
+    int selectUserIsExist(@Param("email") String email, @Param("phone") String phone);
+
+    /**
+     * 新增教师头像
+     * @param map 教师信息
+     * @return 新增结果
+     */
+    int insertTeacherPhoto(Map<String, Object> map);
+
+    /**
+     * 新增教师
+     * @param map 教师信息
+     * @return 新增结果
+     */
+    int insertTeacher(Map<String, Object> map);
+
+    /**
+     * 新增教师与用户的中间表
+     * @param map 教师信息
+     * @return 新增结果
+     */
+    int insertTeacherAndUser(Map<String, Object> map);
+}
