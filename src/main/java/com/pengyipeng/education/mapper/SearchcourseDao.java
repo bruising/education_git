087d2c2c4ddbde566b55fe87f5929283b309c882
韩帅龙 <2297507092@qq.com>
2020-01-08 14:30:38 +0800

    课程显示和排序

--- src/main/java/com/pengyipeng/education/mapper/SearchcourseDao.java
+++ src/main/java/com/pengyipeng/education/mapper/SearchcourseDao.java
@@ -1,6 +1,7 @@
 package com.pengyipeng.education.mapper;
 
 
+import com.pengyipeng.education.model.entity.Course;
 import com.pengyipeng.education.model.vo.CourseStudentVo;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
@@ -35,5 +36,21 @@ public interface SearchcourseDao {
      * @return
      */
     int lshelfBycourse_id(@Param(value = "course_id") String course_id);
+
+    /**
+     * 查询 修改后的顺序是否存在
+     * @param sort
+     * @return
+     */
+    List<Course> checkByOrder(@Param(value = "sort") String sort);
+
+
+    /**
+     * 修改顺序
+     * @param sort  修改为的顺序
+     * @param course_id  根据courseid修改
+     * @return  返回是否修改成功
+     */
+    int updateBycourseOrder(@Param(value = "sort") String sort,@Param(value = "courseid") String course_id );
 
 }
