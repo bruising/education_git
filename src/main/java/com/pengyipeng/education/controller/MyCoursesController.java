337b9aec3c03c3a27001a1b114f37b0ed271a66f
g18295819495 <50036335+g18295819495@users.noreply.github.com>
2020-01-08 14:45:21 +0800

    gxl测试

---
+++ src/main/java/com/pengyipeng/education/controller/MyCoursesController.java
@@ -0,0 +1,16 @@
+package com.pengyipeng.education.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @author 郭新龙
+ * @version V1.0
+ * @Project education
+ * @Package com.pengyipeng.education.controller
+ * @date 2020/1/8 14:40 星期三
+ * 我的学习中心-我的课程
+ */
+@Controller
+public class MyCoursesController {
+
+}
---
+++ src/main/java/com/pengyipeng/education/mapper/MyCoursesDao.java
@@ -0,0 +1,12 @@
+package com.pengyipeng.education.mapper;
+
+/**
+ * @author 郭新龙
+ * @version V1.0
+ * @Project education
+ * @Package com.pengyipeng.education.mapper
+ * @date 2020/1/8 14:42 星期三
+ */
+public class MyCoursesDao {
+
+}
